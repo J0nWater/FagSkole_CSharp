905b699e80ce311486f613d6132dedb973b98a76
J0nWater <79053067+J0nWater@users.noreply.github.com>
2025-05-20 11:01:55 +0200

    init

---
+++ 1.1_IntroToNet/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = "Hello, World! ";
+int number = 1;
+double number2 = 1.1;
+bool isTrue = true;
+char charA = 'a';
+
+
+Console.WriteLine(text);
+
+var text2 = "Hello, World!";
+var nuber = 1;
+var doubleNumber = 1.1;
+var isTrue2 = true;
+var charA2 = 'a';
+
+var numberText = "2";
+var numberTextAsNumber = Convert.ToInt32(numberText);
+
+
---
+++ 1.1_IntroToNetV2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _1._1_IntroToNetV2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

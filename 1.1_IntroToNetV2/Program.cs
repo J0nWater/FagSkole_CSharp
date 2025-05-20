3bc1512429e07d5e23a4ed05b4479571f77b3855
J0nWater <79053067+J0nWater@users.noreply.github.com>
2025-05-20 14:14:17 +0200

    Commit

--- 1.1_IntroToNetV2/Program.cs
+++ 1.1_IntroToNetV2/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var name = Ask("What is your name? ");
+        var locastion = Ask("Where do you live? ");
+        Console.WriteLine($"Hi, {name}. You live at {locastion}");
+    }
+
+    static string Ask(string question)
+    {
+        Console.Write(question);
+        return Console.ReadLine();
     }
 }

2950cd1c5879bf94003ac06c858615d1189ea4d1
J0nWater <79053067+J0nWater@users.noreply.github.com>
2025-05-30 16:11:44 +0200

    Commit

---
+++ 1.3 MoreIntroToNET/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1._3_MoreIntroToNET;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var name = Ask("What is your name? ");
+        var locastion = Ask("Where do you live? ");
+        Console.WriteLine($"Hi, {name}. You live at {locastion}");
+    }
+
+    static string Ask(string question)
+    {
+        Console.Write(question);
+        return Console.ReadLine();
+    }
+}

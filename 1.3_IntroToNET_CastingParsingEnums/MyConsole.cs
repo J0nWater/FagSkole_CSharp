7216e47d5eba610d0ae24bdaa932b2d06fc693b9
J0nWater <79053067+J0nWater@users.noreply.github.com>
2025-05-30 18:52:56 +0200

    Commit

---
+++ 1.3_IntroToNET_CastingParsingEnums/MyConsole.cs
@@ -0,0 +1,10 @@
+namespace _1._3_IntroToNET_CastingParsingEnums;
+
+public class MyConsole
+{
+    public static string Ask(string question)
+    {
+        Console.Write(question);
+        return Console.ReadLine();
+    }
+}
---
+++ 1.3_IntroToNET_CastingParsingEnums/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _1._3_IntroToNET_CastingParsingEnums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+        // var name = MyConsole.Ask("What is your name? ");
+        // var locastion = MyConsole.Ask("Where do you live? ");
+        // Console.WriteLine($"Hi, {name}. You live at {locastion}");
+    }
+}

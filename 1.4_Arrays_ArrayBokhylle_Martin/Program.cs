aa48c583560cc68f3e0cc967941d385effb84aab
J0nWater <79053067+J0nWater@users.noreply.github.com>
2025-06-01 20:28:01 +0200

    Commit

---
+++ 1.4_Arrays_ArrayBokhylle_Martin/Program.cs
@@ -0,0 +1,18 @@
+﻿// Console.WriteLine("Hello, World!");
+
+string[] array = new string[2];
+array[0] = "1";
+array[1] = "2";
+
+string[] array2 = ["1", "2"];
+
+string[] array3 = new string[]
+{
+    "1",
+    "2"
+};
+
+foreach (var x in array2)
+{
+    Console.WriteLine(x);
+}

3e5a1b20aa5ea6b1ba1d0aed53a818a657681cb0
J0nWater <79053067+J0nWater@users.noreply.github.com>
2025-06-13 10:27:13 +0200

    Commit

---
+++ 1.7_RobustCode/FileManager.cs
@@ -0,0 +1,54 @@
+﻿namespace _1._7_RobustCode;
+
+public class FileManager
+{
+    public string FilePath;
+
+    public FileManager(string filePath)
+    {
+        var directory = Path.GetDirectoryName(filePath);
+        if (!Directory.Exists(directory))
+        {
+            throw new ApplicationException("Directory doesn't exist");
+        }
+        FilePath = filePath;
+    }
+
+    public string ReadFileContent()
+    {
+        if (!File.Exists(FilePath))
+        {
+            return "File not found, create file";
+        }
+
+        try
+        { 
+            return File.ReadAllText(FilePath);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return string.Empty;
+        }
+    }
+
+    public void CreateFile()
+    {
+        File.Create(FilePath).Close();
+    }
+
+    public void WriteFileContent(string content)
+    {
+        File.WriteAllText(FilePath, content);
+    }
+
+    public void MoveFile(string newPath)
+    {
+        File.Move(FilePath, newPath);
+    }
+
+    public void DeleteFile()
+    {
+        File.Delete(FilePath);
+    }
+}
---
+++ 1.7_RobustCode/Program.cs
@@ -0,0 +1,39 @@
+﻿using _1._7_RobustCode;
+
+const string filePath = @"C:\Users\Jon\Documents\test.txt";
+var fileManager = new FileManager(filePath);
+
+
+while (true)
+{
+    Console.WriteLine("Available commands: create, write, read, delete, move");
+    Console.Write("> ");
+    var input = Console.ReadLine();
+    switch (input)
+    {
+        case "read":
+            Console.WriteLine(fileManager.ReadFileContent());
+            break;
+        case "create":
+            fileManager.CreateFile();
+            Console.WriteLine($"File {fileManager.FilePath} created");
+            break;
+        case "write":
+            Console.WriteLine("Content:");
+            var content = Console.ReadLine();
+            fileManager.WriteFileContent(content);
+            break;
+        case "delete":
+            fileManager.DeleteFile();
+            Console.WriteLine($"File {fileManager.FilePath} deleted");
+            break;
+        case "move":
+            Console.Write("New path: ");
+            var newPath = Console.ReadLine();
+            fileManager.MoveFile(newPath);
+            break;
+        case "exit":
+            Environment.Exit(0);
+            break;
+    }
+}
